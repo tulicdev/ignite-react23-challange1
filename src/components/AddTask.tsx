e38c06d5c05ed007a9892b5f32013633b633a6f7
Tulic <tulic.dev@gmail.com>
2023-10-23 20:37:42 -0300

    feat(addtask): add interface, states and handlers

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,14 +1,41 @@
+import { useState } from "react";
 import Plus from "../assets/plus.svg?react";
 import style from "./AddTask.module.css";
 
+interface Task {
+  description: string;
+  isDone: boolean;
+}
+
 export function AddTask() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTaskDescription, setNewTaskDescription] = useState("");
+
+  function handleNewTaskChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setNewTaskDescription(event.target.value);
+    console.log(newTaskDescription);
+  }
+
+  function handleCreateNewTask(
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) {
+    event.preventDefault();
+    const task = { description: newTaskDescription, isDone: false };
+    setTasks((state) => [...state, task]);
+    setNewTaskDescription('');
+  }
+
   return (
-    <section className={style.container}>
-      <textarea placeholder="Adicionar uma nova tarefa" />
-      <button>
+    <form className={style.container}>
+      <textarea
+        required
+        onChange={handleNewTaskChange}
+        placeholder="Adicionar uma nova tarefa"
+      />
+      <button type="submit" onClick={handleCreateNewTask}>
         <span>Criar</span>
         <Plus />
       </button>
-    </section>
+    </form>
   );
 }
